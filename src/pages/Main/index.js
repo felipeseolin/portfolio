d8e83778f1f83ed1cfea54d3bfa8b1ca5ce688a3
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-06-10 16:39:26 -0300

    Site Skeleton

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import "./styles.scss";
 import Nav from "../../components/Nav";
 
@@ -6,7 +6,61 @@ const Main = () => {
     return (
         <Fragment>
             <Nav/>
-            <p>Meu Portfolio</p>
+
+            <section id="home" className="hero flex center-all blue-lagoon">
+                <h1>Talvez vai alguma frase</h1>
+            </section>
+
+            <section id="about-me" className="flex nowrap center-all">
+                <img src="https://picsum.photos/id/565/500/500" alt="Foto do Felipe Seolin Bento"/>
+                <div>
+                    <h1>Felipe Seolin Bento</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci delectus dolorem
+                        dolores eos facilis ipsa, maxime neque nobis perferendis placeat quo sint, suscipit tempora
+                        voluptatem voluptates! Culpa, facere.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci delectus dolorem
+                        dolores eos facilis ipsa, maxime neque nobis perferendis placeat quo sint, suscipit tempora
+                        voluptatem voluptates! Culpa, facere.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci delectus dolorem
+                        dolores eos facilis ipsa, maxime neque nobis perferendis placeat quo sint, suscipit tempora
+                        voluptatem voluptates! Culpa, facere.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci delectus dolorem
+                        dolores eos facilis ipsa, maxime neque nobis perferendis placeat quo sint, suscipit tempora
+                        voluptatem voluptates! Culpa, facere.</p>
+
+                    <a href="https://google.com">Baixe meu currículo aqui</a>
+                </div>
+            </section>
+
+            <section id="details">
+                <div id="interests"></div>
+
+                <div id="languages"></div>
+
+                <div id="formation"></div>
+
+                <div id="developed-activities"></div>
+            </section>
+
+            <section id="skills">
+
+            </section>
+
+            <section id="blog"></section>
+
+            <section id="works"></section>
+
+            <section id="more">
+                <div id="biography"></div>
+                <div id="work-review"></div>
+            </section>
+
+            <section id="contacts">
+                <div id="social-media"></div>
+                <div id="form"></div>
+            </section>
+
+            <footer></footer>
         </Fragment>
     );
 };
