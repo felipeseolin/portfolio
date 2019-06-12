7a110cb006de87549d4f88c056af3891836bf769
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-06-12 16:50:48 -0300

    Updates

---
+++ precache-manifest.3bcfa9791e2b4a56e8597ad7eeabcc67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f79704e7f62b6f98e69bd2d739aae5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c8eba05f8aa568aff85",
+    "url": "/portfolio/static/css/main.ceb886bf.chunk.css"
+  },
+  {
+    "revision": "e55d5c710a4b6f06d6dd",
+    "url": "/portfolio/static/js/2.8a83ae99.chunk.js"
+  },
+  {
+    "revision": "7c8eba05f8aa568aff85",
+    "url": "/portfolio/static/js/main.d33bdaef.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
